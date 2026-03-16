4b73bc2b25bb18517eb30ff472735a8aee3a5180
penguinjean0421 <penguinjean0421@gmail.com>
2026-03-16 12:17:29 +0900

    ✨ feat : Add aespa member responses and random choice command
    - Implement automated Instagram handle and chant responses for aespa members
    - Add choose command to select a random option from multiple inputs
    - Include bot.process_commands in on_message to prevent command conflicts

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
+import random
 
 # 토큰 가져오기
 load_dotenv()
@@ -22,5 +23,30 @@ bot = commands.Bot(bot_command_prefix, intents = intents, help_command=None)
 async def on_ready(): 
     print(f'{bot.user.name} 봇이 온라인이 되었습니다!')
     await bot.change_presence(status=discord.Status.online, activity=discord.Game("penguinjæn Online"))
+
+@bot.event
+# 이름 입력시 구호나 인스타로 응답
+async def on_message(message) :
+    if message.content.startswith('aespa') or message.content.startswith('에스파'):
+        await message.channel.send('Be my æ')
+    if message.content.startswith('karina') or message.content.startswith('카리나') :
+        await message.channel.send('@katarinabluu')
+    if message.content.startswith('giselle') or message.content.startswith('지젤') :
+        await message.channel.send('@aerichandesu')
+    if message.content.startswith('winter') or message.content.startswith('윈터') :
+        await message.channel.send('@imwinter')
+    if message.content.startswith('ningning') or message.content.startswith('닝닝') :
+        await message.channel.send('@imnotningning')
+    await bot.process_commands(message)
+
+@bot.command()
+# 제시된 것중에 하나 선택
+async def choose(ctx, *options) : 
+    print('입력중')
+    if len (options) < 2 :
+        await ctx.send ("최소 2개이상의 선택지를 제시해라 좀.")
+    else :
+        select = random.choice(options)
+        await ctx.send(f'{select}')
 
 bot.run(BOT_TOKEN)
